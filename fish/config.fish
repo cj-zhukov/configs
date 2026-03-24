c6becd7bb987195ac78610877521d241de60cab9
Sergey Zhukov <cjthebest89@gmail.com>
2026-03-24 09:25:20 +0400

    fix path in config with no quotes

--- fish/config.fish
+++ fish/config.fish
@@ -7,39 +7,22 @@ set fish_greeting ""
 
 # Print a new line after any command
 # source ~/.config/fish/functions/postexec_newline.fish
+# source ~/.config/fish/functions/postexec_newline.fish
 
 # Show the full path
 # set fish_prompt_pwd_dir_length 0
 
 # Add path
-fish_add_path /usr/local/Cellar/
-fish_add_path /Library/PostgreSQL/13/bin
-fish_add_path "~/utils"
-
-# Setup brew
-# eval "$(/opt/homebrew/bin/brew shellenv)"
+fish_add_path ~/utils
+fish_add_path ~/.cargo/bin
 
 # Enable Starship prompt
-# starship init fish | source
-eval "$(starship init fish)"
+starship init fish | source
+# eval "$(starship init fish)"
 
 # Aliases
+alias hx helix
+# alias ll "exa --grid --long --classify --all --header --no-user --tree --level 1 --group-directories-first"
 alias ll "exa --grid --long --classify --header --no-user --tree --level 1 --group-directories-first"
 alias lll "exa --grid --long --classify --all --header --no-user --tree --level 1 --group-directories-first"
 alias lsd "du -hs"
-alias myvenv 'source .env/bin/activate.fish'
-
-# `ls` → `ls -laG` abbreviation
-# abbr -a -g ls ls -laG
-
-# `ls` → `exa` abbreviation
-# Requires `brew install exa`
-# if type -q exa
-#   abbr --add -g ls 'exa --long --classify --all --header --git --no-user --tree --level 1'
-# end
-
-# `cat` → `bat` abbreviation
-# Requires `brew install bat`
-# if type -q bat
-#   abbr --add -g cat 'bat'
-# end
